5b06301b3e8c2c3ed8ae0c334dfe5d6b4297f3cc
Denis Duev <denis_duev@abv.bg>
2019-01-06 16:11:45 +0200

    initial commit

---
+++ src/main/java/com/fmi/studentnews/StudentNewsApplication.java
@@ -0,0 +1,14 @@
+package com.fmi.studentnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentNewsApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/fmi/studentnews/StudentNewsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.fmi.studentnews;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentNewsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
